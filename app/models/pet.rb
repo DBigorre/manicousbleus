1bd4597552828282ea6f8354c21879483ba9249c
desiree bigorre <bigorredesiree@gmail.com>
2022-11-07 17:07:00 -0500

    creation du model album avec reference pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,4 @@
 class Pet < ApplicationRecord
+  has_many :albums
   belongs_to :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_215852) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_220358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "photos"
+    t.bigint "pet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pet_id"], name: "index_albums_on_pet_id"
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "species"
@@ -46,5 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_215852) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "albums", "pets"
   add_foreign_key "pets", "users", column: "users_id"
 end
